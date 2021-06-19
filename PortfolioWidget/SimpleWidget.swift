20b9f29aea9acc6f7bb52498b0059aabd94bbc06
Krisda Siangchaew <krisda.siangchaew@gmail.com>
2021-06-19 10:30:47 +0700

    Add simple and complex widgets

---
+++ PortfolioWidget/SimpleWidget.swift
@@ -0,0 +1,39 @@
+//
+//  SimpleWidget.swift
+//  PortfolioWidgetExtension
+//
+//  Created by Krisda on 19/6/2564 BE.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct SimplePortfolioWidget: Widget {
+    let kind: String = "SimplePortfolioWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            PortfolioWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("Up next…")
+        .description("Your #1 top-priority item.")
+        .supportedFamilies([.systemSmall])
+    }
+}
+
+struct PortfolioWidgetEntryView: View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            Text("Up next...")
+                .font(.title)
+
+            if let item = entry.items.first {
+                Text(item.itemTitle)
+            } else {
+                Text("Nothing!")
+            }
+        }
+    }
+}
